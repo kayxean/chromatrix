ea8e2af78937580364cb0f64849f6fdd871481aa
kayxean <36327242+kayxean@users.noreply.github.com>
2026-02-22 21:55:58 +0700

    chore: prepare for publish (#32)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
   },
+  format: {
+    esm: {
+      target: ['es2022'],
+    },
+    cjs: {
+      target: ['node20'],
+    },
+  },
+  dts: true,
   minify: true,
   treeshake: true,
 });
